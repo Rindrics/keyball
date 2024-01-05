81d6c406d93bbd598eb40990dd2424d9c79a364c
Akira Hayashi <smxshxishxad@me.com>
2024-01-05 10:46:23 +0900

    Move KC_TAB lower to make it easily accessible

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
@@ -413,7 +413,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DVORAK] = LAYOUT_universal(
     KC_TAB    , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y         ,                                  KC_F   , KC_G    , KC_C    , KC_R    , KC_L    , KC_TILDE ,
     LCTL_LANG , KC_A    , KC_O    , KC_E    , KC_U    , KC_I         ,                                  KC_D   , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS  ,
-    KC_LSFT   , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X         ,                                  KC_B   , KC_M    , KC_W    , KC_V    , KC_Z    , KC_RSFT  ,
+    LSFT_T(KC_TAB)   , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X         ,                                  KC_B   , KC_M    , KC_W    , KC_V    , KC_Z    , KC_RSFT  ,
                           KC_CAPS , KC_LALT , KC_LGUI , LCTL_T(KC_SPC) , LT(2,KC_ESC),            LT(3,KC_ENT) , KC_SPC  ,           _______ , _______ , KC_RGUI
   ),
 
