9079285a44daae59c173bd7e5551e08f039c048e
Akira Hayashi <smxshxishxad@me.com>
2024-01-13 23:04:41 +0900

    Name two layers to make them understandable

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
@@ -22,6 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum custom_layers {
   DVORAK_NICOLA,
+  NUMBER,
+  SYMBOL
 };
 
 enum custom_keycodes {
@@ -439,17 +441,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SLSH , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y    ,                                     KC_F    , KC_G    , KC_C    , KC_R    , KC_L            , KC_TILDE ,
     KC_TAB  , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    ,                                     KC_D    , KC_H    , KC_T    , KC_N    , KC_S            , KC_MINS  ,
     KC_RSFT , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X    ,                                     KC_B    , KC_M    , KC_W    , KC_V    , KC_Z            , KC_RSFT  ,
-                        _______ , KC_LALT , KC_LGUI , KC_LCTL , LT(2,KC_NO),        LT(3,KC_ENT) ,  KC_SPC  ,           _______ , _______ , RGUI_T(KC_BSPC)
+                        _______ , KC_LALT , KC_LGUI , KC_LCTL , LT(NUMBER,KC_NO),         LT(SYMBOL,KC_ENT) ,  KC_SPC  ,           _______ , _______ , RGUI_T(KC_BSPC)
   ),
 
-  [1] = LAYOUT_universal(
+  [NUMBER] = LAYOUT_universal(
     _______  , _______ , _______ , _______ , _______ , _______ ,                                       _______ , _______ , _______ , _______ , _______ , _______ ,
     _______  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                       KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ ,
     _______  , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_4) , S(KC_5) ,                                       S(KC_6) , S(KC_7) , S(KC_8) , S(KC_9) , S(KC_0) , _______  ,
                          _______ , _______ , _______ , _______ , _______ ,                   _______ , _______ ,           _______ , _______ , _______
   ),
 
-  [2] = LAYOUT_universal(
+  [SYMBOL] = LAYOUT_universal(
     KC_BSLS  , _______ , KC_GRAVE , KC_QUOT , KC_QUES , _______ ,                                       _______ , _______ , KC_UP   , _______  , _______ , _______ ,
     _______  , KC_LCBR , KC_RCBR  , KC_LPRN , KC_RPRN , KC_PIPE ,                                       _______ , KC_LEFT , KC_DOWN , KC_RIGHT , _______ , _______ ,
     _______  , KC_LBRC , KC_RBRC  , KC_LABK , KC_RABK , KC_PLUS ,                                       _______ , _______ , _______ , _______  , _______ , _______ ,
