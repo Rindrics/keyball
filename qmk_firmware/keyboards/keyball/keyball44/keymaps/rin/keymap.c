bdbbe436eb5d8cace7ee850643b0cc44d319fb97
Akira Hayashi <smxshxishxad@me.com>
2024-01-26 21:00:08 +0900

    Align keymap

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
@@ -461,16 +461,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DVORAK_NICOLA] = LAYOUT_universal(
-    KC_TILDE , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y    ,                                     KC_F     , KC_G    , KC_C    , KC_R    , KC_L    , KC_ENT   ,
-    KC_TAB   , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    ,                                     KC_D     , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS  ,
-    KC_RSFT  , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X    ,                                     KC_B     , KC_M    , KC_W    , KC_V    , KC_Z    , KC_RSFT  ,
+    KC_TILDE , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y    ,                                                KC_F    , KC_G    , KC_C    , KC_R    , KC_L    , KC_ENT   ,
+    KC_TAB   , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    ,                                                KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS  ,
+    KC_RSFT  , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X    ,                                                KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , KC_RSFT  ,
                        LSG(KC_4) , KC_LGUI , KC_LALT , KC_LCTL , LT(NUMBER,KC_SLSH),           LSFT_T(KC_TAB) , KC_SPC  ,           _______ , _______ , RGUI_T(KC_BSPC)
   ),
 
   [NUMBER] = LAYOUT_universal(
-    _______  , _______ , _______ , _______ , _______ , _______ ,                                       _______ , _______ , _______ , _______ , _______ , _______ ,
-    _______  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                       KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ ,
-    _______  , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_4) , S(KC_5) ,                                       S(KC_6) , S(KC_7) , S(KC_8) , S(KC_9) , S(KC_0) , _______  ,
+    _______  , _______ , _______ , _______ , _______ , _______ ,                                                 _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                                 KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ ,
+    _______  , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_4) , S(KC_5) ,                                                 S(KC_6) , S(KC_7) , S(KC_8) , S(KC_9) , S(KC_0) , _______ ,
                          _______ , _______ , _______ , _______ , _______ ,                  LT(SYMBOL,KC_BSLS) , _______ ,           _______ , _______ , _______
   ),
 
